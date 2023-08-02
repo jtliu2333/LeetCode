dee40d8a785e8d25e5712f736cfd6c799adcc03d
TLv <jtliu2333@163.com>
2023-08-02 17:10:15 +0800

    链表2

--- Code1/Code1/Program.cs
+++ Code1/Code1/Program.cs
@@ -10,5 +10,77 @@ namespace MyCode // Note: actual namespace depends on the project name.
         {
             Console.WriteLine("Hello World!");
         }
+        //链表
+        //Definition for singly-linked list.
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int x) { val = x; }
+        }
+        //public class Solution
+        //{
+        //160
+        public ListNode GetIntersectionNode(ListNode headA, ListNode headB)
+        {
+            int lengthA = GetListLength(headA);
+            int lengthB = GetListLength(headB);
+            int delta = Math.Abs(lengthB - lengthA);
+            ListNode curA= headA;
+            ListNode curB = headB;
+            if (lengthA < lengthB)
+            {
+                curB = headB;
+                while (delta > 0 && curB != null)
+                {
+                    curB = curB.next;
+                    delta--;
+                }
+            }
+            else if (lengthA > lengthB)
+            {
+                curA = headA;
+                while (delta > 0 && curA != null)
+                {
+                    curA = curA.next;
+                    delta--;
+                }
+            }
+            while (curA != null && curB != null && curA != curB)
+            {
+                curA= curA.next;
+                curB= curB.next;
+            }
+            return curA;
+        }
+        public int GetListLength(ListNode l)
+        {
+            ListNode cur = l;
+            int length = 0;
+            while (cur != null)
+            {
+                length = length + 1;
+                cur = cur.next;
+            }
+            return length;
+        }
+        //}
+        //206
+        public ListNode ReverseList(ListNode head)
+        {
+            if (head == null)
+                return null;
+            ListNode cur = head;
+            ListNode pre = null;
+            ListNode next = null;
+            while (cur!= null)
+            {
+                next = cur.next;
+                cur.next = pre;
+                pre = cur;
+                cur = next;
+            }
+            return pre;
+        }
     }
 }
